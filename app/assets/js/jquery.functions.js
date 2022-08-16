daf39a6f9a45d56b4be7736401bbbb609344bece
Jhomar Nando <contato@joekyy.com.br>
2022-08-15 21:23:49 -0300

    terminando o header

--- app/assets/js/jquery.functions.js
+++ app/assets/js/jquery.functions.js
@@ -1,3 +1,19 @@
 $(function () {
+	$('.nav-button').click(function(){
+		if (!$(this).hasClass('is-active')) {
+			$(this).addClass('is-active');
+			$('.menu nav').addClass('nav-menu--open');
+			$('.menu nav').removeClass('nav-menu--close');
+		} else {
+			$(this).removeClass('is-active');
+			$('.menu nav').removeClass('nav-menu--open');
+			$('.menu nav').addClass('nav-menu--close');
+		}
+	});
 
+	$('header .menu nav ul li a').click(function(){
+		$('.nav-button').removeClass('is-active');
+		$('.menu nav').removeClass('nav-menu--open');
+		$('.menu nav').addClass('nav-menu--close');
+	});
 });
